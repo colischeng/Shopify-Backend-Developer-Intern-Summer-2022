8cdc0bedf21e55e615570e166d0af12b8d92b199
Colis Cheng <colis_cheng@thecdm.ca>
2022-01-10 21:53:35 -0800

    Performs delete requests

---
+++ server/static/delete.js
@@ -0,0 +1,18 @@
+const deleteButtons = document.getElementsByClassName("delete");
+
+for (let i = 0; i < deleteButtons.length; i++) {
+  const deleteButton = deleteButtons[i];
+
+  deleteButton.addEventListener("click", async () => {
+    await fetch(`/api/inventory/${deleteButton.id}`, {
+      method: "DELETE",
+    }).then((res) => {
+      if (res.status < 400) {
+        alert("Delete was successful");
+        window.location.href = "http://localhost:3000/";
+      } else {
+        alert("Delete was unsuccessful please retry");
+      }
+    });
+  });
+}
